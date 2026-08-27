5b26b6266e2101edbe11c5a097bcbf30838233ab
Skygress <skyibris@gmail.com>
2026-08-27 23:46:33 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 import os
 from dotenv import load_dotenv
-import json
 
 load_dotenv()
 
 class Config:
     TOKEN = os.getenv('BOT_TOKEN')
-    DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///crypto_tycoon.db')
     ADMIN_ID = os.getenv('ADMIN_ID')
     
     # Game settings
